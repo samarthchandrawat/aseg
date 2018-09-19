33fac80cdadf3d5e5f3202257b59940b81c3c182
samyak-jain <samtan106@gmail.com>
2018-09-19 12:12:29 +0000

    Changes for environment

---
+++ utils/CMakeFiles/patches.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/patches.dir/src/devide_to_patches.cpp.o"
+  "patches.pdb"
+  "patches.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/patches.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- utils/build_old.sh
+++ utils/build_old.sh
@@ -4,8 +4,8 @@ cmake \
 -DPYTHON_INCLUDE_DIR=$PYTHON_DIR/include/python3.5m \
 -DPYTHON_INCLUDE_DIR2=$PYTHON_DIR/include \
 -DBoost_INCLUDE_DIR=/usr/local/include \
--DBoost_NumPy_INCLUDE_DIR=/opt/conda/include \
--DBoost_NumPy_LIBRARY_DIR=/opt/conda/lib \
+-DBoost_NumPy_INCLUDE_DIR=/usr/local/include \
+-DBoost_NumPy_LIBRARY_DIR=/usr/local/lib64 \
 -DOpenCV_DIR=/usr/local/share/OpenCV \
 -Wno-dev \
 . && make
